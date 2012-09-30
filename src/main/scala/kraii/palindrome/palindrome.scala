402afca3accc5c7adb0647e6269138f155f44ed7
kraii <matthewkerry@gmail.com>
2012-10-01 00:23:35 +0100

    Fixed formatting

--- src/main/scala/kraii/palindrome/palindrome.scala
+++ src/main/scala/kraii/palindrome/palindrome.scala
@@ -2,14 +2,14 @@ package kraii
 
 import scala.annotation.tailrec
 package object palindrome {
-	def longestPalindrome(text: String): String = {
-	  if(text.isEmpty()) ""
-	  else if(text == text.trim.reverse) text
-	  else longestOf(longestPalindrome(text.tail), longestPalindrome(text.reverse.tail))
-	}
-	
-	private def longestOf(s1: String, s2: String): String = {
-	  if(s1.length() >= s2.length) s1
-	  else s2
-	}
+  def longestPalindrome(text: String): String = {
+    if (text.isEmpty()) ""
+    else if (text == text.trim.reverse) text
+    else longestOf(longestPalindrome(text.tail), longestPalindrome(text.reverse.tail))
+  }
+
+  private def longestOf(s1: String, s2: String): String = {
+    if (s1.length() >= s2.length) s1
+    else s2
+  }
 }
--- src/test/scala/kraii/palindrome/PalindromeSpec.scala
+++ src/test/scala/kraii/palindrome/PalindromeSpec.scala
@@ -4,23 +4,23 @@ import org.scalatest.FlatSpec
 import org.scalatest.matchers.ShouldMatchers
 
 class PalindromeSpec extends FlatSpec with ShouldMatchers {
-	"longestPalindrome" should "give empty string for empty string" in {
-	  longestPalindrome("") should equal ("")
-	}
-	
-	it should "give a or b for ab" in {
-	  longestPalindrome("ab") should (equal ("a") or equal("b"))
-	}
-	
-	it should "give racecar for racecar" in {
-		longestPalindrome("racecar") should equal ("racecar")
-	}
-	
-	it should "give racecar for racecarcat" in {
-		longestPalindrome("racecarcat") should equal ("racecar")
-	}
-	
-	it should "should not include whitespace" in {
-		longestPalindrome(" racecar ") should equal ("racecar")
-	}
+  "longestPalindrome" should "give empty string for empty string" in {
+    longestPalindrome("") should equal("")
+  }
+
+  it should "give a or b for ab" in {
+    longestPalindrome("ab") should (equal("a") or equal("b"))
+  }
+
+  it should "give racecar for racecar" in {
+    longestPalindrome("racecar") should equal("racecar")
+  }
+
+  it should "give racecar for racecarcat" in {
+    longestPalindrome("racecarcat") should equal("racecar")
+  }
+
+  it should "should not include whitespace" in {
+    longestPalindrome(" racecar ") should equal("racecar")
+  }
 }
